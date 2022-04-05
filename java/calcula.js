a9dde5a82670b1f9005a728846ee1207e2e273fe
lacruzr <lacruzr@hotmail.com>
2022-04-04 21:10:39 -0700

    creando repositorio de pagina web de calculadora de impuestos

---
+++ java/calcula.js
@@ -0,0 +1,60 @@
+function calculadora(){
+    document.getElementById("pantalla").innerHTML =  "resolviendo";
+}
+function punto(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}.`;
+
+}function seven(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}7`;
+}
+
+function eight(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}8`;
+}
+
+function nine(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}9`;
+}
+
+function one(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}1`;
+}
+
+function two(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}2`;
+}
+function three(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}3`;
+}
+function four(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}4`;
+}
+function five(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}5`;
+}
+function six(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}6`;
+}
+function cero(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}0`;
+}
+function dcero(){
+    var texto = (document.getElementById("pantalla").value);
+    document.getElementById("pantalla").innerHTML =  `${texto}00`;
+}
+function clearp(){
+    var texto = "";
+    document.getElementById("pantalla").innerHTML =  `${texto}`;
+    
+}
